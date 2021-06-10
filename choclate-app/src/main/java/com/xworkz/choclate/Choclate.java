051e39f1e9c772023c665e972ce3c62418b2862e
appu22 <appu.xworkz@gmail.com>
2021-06-10 22:15:08 +0530

    using constructor and setters methods

--- choclate-app/src/main/java/com/xworkz/choclate/Choclate.java
+++ choclate-app/src/main/java/com/xworkz/choclate/Choclate.java
@@ -1,20 +1,27 @@
 package com.xworkz.choclate;
 
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
 @ToString
+@RequiredArgsConstructor
 
 public class Choclate {
 
 	private int id;
+	@NonNull
 	private String name;
+	@NonNull
 	private String brand;
+	@NonNull
 	private double price;
 
+	
 	public Choclate() {
 
 		System.out.println(getClass().getName() + " Choclate  created   ");
--- choclate-app/src/main/java/com/xworkz/choclate/ChoclateTester.java
+++ choclate-app/src/main/java/com/xworkz/choclate/ChoclateTester.java
@@ -6,17 +6,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ChoclateTester {
 
 	public static void main(String[] args) {
-		String xmlPath = "springConfiguartion.xml";
-		ApplicationContext applicationContext = new ClassPathXmlApplicationContext(xmlPath);
+//		String configLocation = "applicationContext.xml";
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 
 		Choclate choclate = applicationContext.getBean(Choclate.class);
 
-		choclate.setName("kismi");
-		choclate.setBrand("parale");
-		choclate.setPrice(0.50);
-		System.out.println("Choclate Name :  "+choclate.getName() +"\n Choclate Brand Name : "
-				+ ""+ choclate.getBrand() +"\n Choclate Price : "+choclate.getPrice()) ;
-		
+//		choclate.setName("kismi");
+//		choclate.setBrand("parale");
+//		choclate.setPrice(0.50);
+//		System.out.println("Choclate Name :  "+choclate.getName() +"\n Choclate Brand Name : "
+//				+ ""+ choclate.getBrand() +"\n Choclate Price : "+choclate.getPrice()) ;
+//		
+		System.out.println(choclate);
 		choclate.taste();
 
 	}
